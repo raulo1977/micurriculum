f2328d33682dcfa65d5ecf5795be7fc0d810b42d
Raul <raulo.rodriguez@gmail.com>
2023-02-23 21:46:36 -0300

    Avanzando script

--- files/script.js
+++ files/script.js
@@ -1,5 +1,6 @@
 
 enlaces=document.getElementsByClassName("menulink"); 
+secciones=document.getElementsByTagName("section");
 
 
 
@@ -11,8 +12,12 @@ enlaces=document.getElementsByClassName("menulink");
 
       function ocultarMostrar(enlace){
         if (!enlaces[enlace].classList.contains("activo")){
-             for (let i = 0; i < enlaces.length; i++) {enlaces[i].classList.remove("activo")}
-             enlaces[enlace].classList.add("activo")
+             for (let i = 0; i < enlaces.length; i++) {
+                enlaces[i].classList.remove("activo");
+                secciones[i].classList.remove("mostrar");
+                secciones[i].classList.add("ocultar");}
+             enlaces[enlace].classList.add("activo");
+                          secciones[enlace].classList.add("mostrar")
             
          }
 
