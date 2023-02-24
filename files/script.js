5d675f95e187d1806239904ee874dd329f84f5e2
Raul <raulo.rodriguez@gmail.com>
2023-02-24 19:29:57 -0300

    actualizacion de estilos

--- files/script.js
+++ files/script.js
@@ -1,26 +1,18 @@
+const enlaces = document.getElementsByClassName("menulink");
+const secciones = document.getElementsByTagName("section");
 
-enlaces=document.getElementsByClassName("menulink"); 
-secciones=document.getElementsByTagName("section");
+for (let i = 0; i < enlaces.length; i++) {
+    enlaces[i].addEventListener('click', function (event) { event.preventDefault(); ocultarMostrar(i) })
+}
 
-
-
-     for (let i = 0; i < enlaces.length; i++) {
-        enlaces[i].addEventListener('click', function (event) 
-        {event.preventDefault();ocultarMostrar(i)})
-        
-      }    
-
-      function ocultarMostrar(enlace){
-        if (!enlaces[enlace].classList.contains("activo")){
-             for (let i = 0; i < enlaces.length; i++) {
-                enlaces[i].classList.remove("activo");
-                secciones[i].classList.remove("mostrar");
-                secciones[i].classList.add("ocultar");}
-             enlaces[enlace].classList.add("activo");
-                          secciones[enlace].classList.add("mostrar")
-            
-         }
-
-
-       
-      }
+function ocultarMostrar(enlace) {
+    if (!enlaces[enlace].classList.contains("activo")) {
+        for (let i = 0; i < enlaces.length; i++) {
+            enlaces[i].classList.remove("activo");
+            secciones[i].classList.remove("mostrar");
+            secciones[i].classList.add("ocultar");
+        }
+        enlaces[enlace].classList.add("activo");
+        secciones[enlace].classList.add("mostrar")
+    }
+}
